16287df6fb457a7b2d0132f2210d876a40993fff
LuthGom <lucianomendes23@yahoo.com>
2022-05-23 21:35:43 -0300

    Atividades referentes à aula nova do módulo 5: Criamos um componente Pokedex.jsx para realizar requisições api de pokeapi. E criamos um PokemonCard.jsx, para executarmos para cada indice do array de retorno da requisiçaão.

---
+++ src/components/Pokedex.jsx
@@ -0,0 +1,38 @@
+// importamos useEffect do próprio React. O useEffect é um hook que nos ajuda a executar efeitos colaterais de forma mais controlada.
+import React, { useEffect, useState } from "react";
+import PokemonCard from "./PokemonCard";
+export default function Pokedex() {
+  const [dados, setDados] = useState([]);
+  //   O hook aceita dois argumentos: uma callback obrigatória onde escrevemos a função ou funções a serem executadas. E  segundo argumento é um array de dependências opcional que vigia o seguinte: o argumento passado dele será vigiado pelo useEffect e, caso ele sofra uma alteração, o useEffect re-executará a função dentro dele, ou seja, o efeito que ele controla/executa.
+  useEffect(() => {
+    // aqui estamos realizando a requisição get a ser executada pelo useEffect quando a página renderiza.
+    try {
+      fetch("https://pokeapi.co/api/v2/pokemon")
+        .then((response) => response.json())
+        .then((data) => {
+          //estamos passando os dados de retorno da API para a função de atualização do state dados.
+          setDados(data.results);
+          console.log(dados);
+        });
+    } catch (erro) {
+      console.log("erro: ", erro);
+    }
+    //aqui estamos passando um array de dependencias vazios como segundo argumento.
+  }, []);
+  //   console.log(dados);
+  return (
+    <div>
+        {/* aqui, estamos iterando sobre o state dados, e passando um parametro pokemon, que represente cada indice do array (que é um array de objetos). */}
+      {dados.map((pokemon) => {
+        // Quando iteramos esse array de objetos utilizando o map, nós copiamos todo esse array e retornamos para cada indice uma lista, onde cada item que atender a condição do retorno (ter os atributos nome e url), vai ser renderizado na nossa tela. Estamos passando um componente PokemonCard, que possui props para receber os valores esperados. Por esse componente estar dentro de um array, e porque pokemon representa cada indice do array(cada objeto), então ele será replicado para cada indice que atende a esse retorno.
+        return (
+          <PokemonCard
+            key={pokemon.name}
+            name={pokemon.name}
+            url={pokemon.url}
+          />
+        );
+      })}
+    </div>
+  );
+}

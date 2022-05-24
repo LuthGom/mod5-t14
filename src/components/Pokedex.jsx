0f64d909629117b9b9db39e996a29a847149b0a4
LuthGom <lucianomendes23@yahoo.com>
2022-05-23 21:43:34 -0300

    Atividades referentes à aula nova do módulo 5: add comentário sobre o state Dados

--- src/components/Pokedex.jsx
+++ src/components/Pokedex.jsx
@@ -2,6 +2,7 @@
 import React, { useEffect, useState } from "react";
 import PokemonCard from "./PokemonCard";
 export default function Pokedex() {
+  // aqui temos um state que inicia como um array vazio para receber os valores de retorno da nossa requisição, logo abaixo.
   const [dados, setDados] = useState([]);
   //   O hook aceita dois argumentos: uma callback obrigatória onde escrevemos a função ou funções a serem executadas. E  segundo argumento é um array de dependências opcional que vigia o seguinte: o argumento passado dele será vigiado pelo useEffect e, caso ele sofra uma alteração, o useEffect re-executará a função dentro dele, ou seja, o efeito que ele controla/executa.
   useEffect(() => {
@@ -22,7 +23,7 @@ export default function Pokedex() {
   //   console.log(dados);
   return (
     <div>
-        {/* aqui, estamos iterando sobre o state dados, e passando um parametro pokemon, que represente cada indice do array (que é um array de objetos). */}
+      {/* aqui, estamos iterando sobre o state dados, e passando um parametro pokemon, que represente cada indice do array (que é um array de objetos). */}
       {dados.map((pokemon) => {
         // Quando iteramos esse array de objetos utilizando o map, nós copiamos todo esse array e retornamos para cada indice uma lista, onde cada item que atender a condição do retorno (ter os atributos nome e url), vai ser renderizado na nossa tela. Estamos passando um componente PokemonCard, que possui props para receber os valores esperados. Por esse componente estar dentro de um array, e porque pokemon representa cada indice do array(cada objeto), então ele será replicado para cada indice que atende a esse retorno.
         return (

5a8f18cf5c6764f8aa0a65971867f4256e8f0e9a
LuthGom <lucianomendes23@yahoo.com>
2022-05-24 21:31:47 -0300

    BuscaPokemon.jsx: criamos um componente que faz uma requisição ao Pokeapi, com valores dinâmicos vindo de input.

--- src/components/BuscaPokemon.jsx
+++ src/components/BuscaPokemon.jsx
@@ -1,12 +1,17 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import Button from "./Button";
 function BuscaPokemon() {
+  // criamos um state que captura o valor do input
   const [value, setValue] = useState("");
+  //   criamos um state que guarda o valor de retorno da requisição.
   const [dados, setDados] = useState([]);
+  //   criamos uma função que fica responsável por fazer uma requisição, utilizando o fetch.
+  // essa requisição aceita um parâmetro que ao final passamos da url, que receberá o nome o pokemon para fazer uma busca direcionada.
   const busca = (e) => {
     fetch(`https://pokeapi.co/api/v2/pokemon/${e}`)
       .then((response) => response.json())
       .then((json) => {
+        //   aqui atribuimos o valor de retorno da requisição ao state dados.
         setDados(json);
         console.log(dados);
       });
@@ -14,7 +19,9 @@ function BuscaPokemon() {
 
   return (
     <div>
+      {/* passamos o setValue para o onChange para atualizar o valor do state value a cada digito. */}
       <input type="text" onChange={(e) => setValue(e.target.value)} />
+      {/* passamos o evento de onClick do Button o e.preventDefault() para evitar o recarregamento da página e passamos a função busca que recebe o value (state que captura o valor do input) como argumento. */}
       <Button
         text="Busca Pokemon"
         click={(e) => {
@@ -22,6 +29,7 @@ function BuscaPokemon() {
           busca(value);
         }}
       />
+      {/* aqui, com o retorno bem-sucedido, estamos pegando valores d retorno da requisição. */}
       <h1>{dados.name}</h1>
       <img src={dados.sprites.front_default} alt="" />
     </div>

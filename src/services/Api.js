0aaa8ac172c3dfeb36b1226140e0cc5902991315
LuthGom <lucianomendes23@yahoo.com>
2022-05-26 22:43:00 -0300

    Api.js: um arquivo de url's para requisições dentro de uma nova camada no projeto chamada services.

---
+++ src/services/Api.js
@@ -0,0 +1,3 @@
+export const apiPokedex = "https://pokeapi.co/api/v2/pokemon";
+export const apiPokemon = (e) => `https://pokeapi.co/api/v2/pokemon/${e}`;
+export const viaCepApi = (cep) => `https://viacep.com.br/ws/${cep}/json/`;

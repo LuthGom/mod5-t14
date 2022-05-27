60c24544df7cffe7261c233f40a877a37f6682b3
LuthGom <lucianomendes23@yahoo.com>
2022-05-26 22:37:43 -0300

    App.jsx: correção do caminho dos componentes importados dentro do arquivo.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import { useContext } from "react";
 import "./App.css";
-import Form from "./components/Form";
+import Form from "./pages/Form/Form";
 // estamos importando os componentes necessários para criar rotas com a lib reac-router-dom.
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import Button from "./components/Button";
 // aqui, importamos o UserContext, pois é ele quem carrega os dados passados no contexto gerado no UserProvider (lembra lá da propriedade value no UserProvider?!)
 import { UserContext } from "./context/UserProvider";
-import Pokedex from "./components/Pokedex";
-import BuscaPokemon from "./components/BuscaPokemon";
-import BuscaCep from "./components/BuscaCep";
+import Pokedex from "./pages/Pokedex/Pokedex";
+import BuscaPokemon from "./pages/BuscaPokemon/BuscaPokemon";
+import BuscaCep from "./pages/BuscaCep/BuscaCep";
 function App() {
   // aqui, estamos desestruturando o dado que queremos do contexto. A sintaxe significa: o dado que queremos sendo instanciado/desestruturado (nesse caso o state auth) = ao hook useContext que recebe no ()  contexto que queremos utilizar.
   const { auth } = useContext(UserContext);
@@ -20,12 +19,8 @@ function App() {
       <h1>Olá, {auth}</h1>
       <BrowserRouter>
         {/* um componente do router que já vem, feito a partir de uma tag <a> com atributo href para direcionar para outro componente no click, desde que a rota seja passada. */}
-        <Link to="/form">Form</Link> 
-        {" "}
-        <Link to="/pokedex">Pokedex</Link>
-        {" "}
-        <Link to="/buscaPokemon">Busca Pokemon</Link>
-        {" "}
+        <Link to="/form">Form</Link> <Link to="/pokedex">Pokedex</Link>{" "}
+        <Link to="/buscaPokemon">Busca Pokemon</Link>{" "}
         <Link to="/buscaCep">Busca Cep</Link>
         {/* componente que engloba todos as rotas devidamente declaradas. */}
         <Routes>

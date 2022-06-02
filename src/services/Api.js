e0fb01aff1c5e78041ff6c132adc08fdb5fe9c10
LuthGom <lucianomendes23@yahoo.com>
2022-06-02 08:55:12 -0300

    Api.js: add url da Apod para realizar requisições

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,3 +1,4 @@
 export const apiPokedex = "https://pokeapi.co/api/v2/pokemon";
 export const apiPokemon = (e) => `https://pokeapi.co/api/v2/pokemon/${e}`;
 export const viaCepApi = (cep) => `https://viacep.com.br/ws/${cep}/json/`;
+export const apodUrl = `https://api.nasa.gov/planetary/apod?api_key=g9LCVFUvZ20Jseh1djh9Wu0RhtnlMB2S6TBLp90g`;
